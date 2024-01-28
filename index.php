34297f7b65f415b2f0376178e119791c39946418
HermantoXYZ <34888878+hermantoXYZ@users.noreply.github.com>
2024-01-28 20:05:50 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,435 @@
+
+<!doctype html>
+<html>
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="shortcut icon" href="https://undangkita.com/gallery/undangkitaicon.png" />
+    <meta name='Author' content='Andi Hermanto-Nur Afifa' />
+
+    
+    <!-- Primary Meta Tags -->
+    <title>Aqiqah Anak Kami - Nazeeya Aynaz Handini</title>
+    <meta name="title" content="Aqiqah Anak Kami - Nazeeya Aynaz Handini">
+    <meta name="description" content="Kepada Yth Bapak/Ibu/Saudara/i untuk berkenan hadir di acara Aqiqah anak kami.">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Aqiqah Anak Kami - Nazeeya Aynaz Handini">
+    <meta name="description" content="Kepada Yth Bapak/Ibu/Saudara/i untuk berkenan hadir di acara Aqiqah anak kami..">
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://undangkita.com/aqiqah-2/">
+    <meta property="twitter:title" content="Aqiqah Anak Kami - Nazeeya Aynaz Handini">
+    <meta name="description" content="Kepada Yth Bapak/Ibu/Saudara/i untuk berkenan hadir di acara Aqiqah anak kami.">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/bootstrap.min.css" />
+    <link rel="stylesheet" href="/font-awesome.min.css" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400,400i,700,700i|Roboto+Slab:400,700|Pacifico|Raleway:400,700,900|Tangerine" />
+    <link rel="stylesheet" href="/style.css" />
+    <link rel="stylesheet" href="/jquery.fancybox.min.css" />
+    <link rel="stylesheet" href="/aos.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+</head>
+
+<body>
+    <div id="home" class="bgcov">
+        <div class="container text-light py-5">
+            <div class="row introcov">
+                <div class="col-12 text-center" data-aos="">
+                    <h6 class="mb-3">UNDANGAN AQIQAH</h6>
+                    <p class="pacifico">Nazeeya Aynaz Handini</p>
+                    <h6>Minggu, 23 Oktober 2022</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="profil" class="py-5 bgimage">
+        <div class="container">
+            <div class="row mb-3">
+                <div class="col-md-8 offset-md-2 text-center mb-3" data-aos="fade-up">
+                    <h5 class="mb-3">بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ</h5>
+                    <p><b>Assalamualaikum Warahmatullahi Wabarakatuh</b></p>
+                    <p>Alhamdulillah puji syukur kehadirat Allah SWT atas berkah dan rahmat-Nya. Sholawat serta salam
+                        tetap tercurah kepada suri tauladan Nabi Muhammad SAW.<br />
+                        Telah lahir puteri pertama kami, yang kami beri nama:
+                    </p>
+                    <div class="card shadow mt-4">
+                        <div class="card-header bgtime">
+                            <div class="row">
+                                <div class="col-12" data-aos="zoom-in">
+                                    <a href="/NAH.PNG" data-fancybox="gallery">
+                                        <img src="http://nazeeyaaynazhandini.site/baby.png" class="rounded shadow mb-3" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body bg-light rounded text-center">
+                            <p class="pacifico2 mt-1 mb-4">Nazeeya Aynaz Handini</p>
+                            <p class="font-weight-bold">Putri Pertama dari <br />
+                                 Andi Hermanto <span class="d-inline-block">& Nur Afifa</span></p>
+                            <p class="mb-0">Lahir di Enrekang, 09 Oktober 2022</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-5 bgsection">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 offset-md-2" data-aos="fade-down">
+                    <p class="text-center" style="font-family: Times New Roman, serif; font-size: 18px"><em>Dari Salman
+                            bin ‘Amir Adl-Dlabiy, ia berkata: Saya mendengar Rasulullah SAW bersabda, “Tiap-tiap anak
+                            itu ada ‘aqiqahnya. Maka sembelihlah binatang ‘aqiqah untuknya dan buanglah kotoran darinya
+                            (cukurlah rambutnya)“.</em>
+                    </p>
+                    <p class="text-center font-weight-bold"
+                        style="font-family: Times New Roman, serif; font-size: 18px">
+                        [HR. Bukhari]
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="acara" class="py-5 bgdate">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 offset-md-2 mb-3 text-center" data-aos="fade-down">
+                    <h2 class="pacifico3 mb-3">Acara</h2>
+                    <p class="text-center">Dengan mengharap rahmat dan ridho Allah SWT,<br />
+                        Kami bermaksud menyelenggarakan Tasyakuran Aqiqah puteri kami, yang Insya Allah akan
+                        diselenggarakan pada:</p>
+                </div>
+                <div class="col-md-8 offset-md-2 mb-4 text-center" data-aos="fade-up">
+                    <div class="rounded shadow p-3">
+                        <h5 class="raleway">Minggu, 23 Oktober 2022</h5>
+                        <p class="mb-1" style="font-size:16px">
+                            Pukul 09.00 WITA - Selesai
+                        </p>
+                        <p style="font-size:16px">
+                            Bertempat di Tontonan, Jalan Poros Cakke-Baraka<br />Kabupaten Enrekang<br /><br />
+                            <a href="#location" class="btn btn-dark">Lihat Lokasi</a>
+                            <a href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=Undangan Aqiqah Sasha&dates=20211231T080000/20211231T110000&ctz=Asia/Makassar&details=Undangan Aqiqah Sasha %0A%0A%0A Lihat Undangan: https://undangkita.com/aqiqah-2/&location=Jl. Perintis Kemerdekaan VII&sf=true&output=xml" class="btn btn-dark">Simpan Acara</a>
+                        </p>
+                        <p>
+                            Note: Tamu Undangan dimohon untuk mematuhi protokol kesehatan.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-5 bgtime">
+        <div class="container" data-aos="fade-down">
+            <ul>
+                <li class="rounded"><span id="days"></span>Days</li>
+                <li class="rounded"><span id="hours"></span>Hours</li>
+                <li class="rounded"><span id="minutes"></span>Minutes</li>
+                <li class="rounded"><span id="seconds"></span>Seconds</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="py-5 bgsection">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 pt-5" data-aos="fade-right">
+                    <div class="card shadow text-dark bg-light">
+                        <div class="card-header text-center">
+                            <h4 class="mt-1" style="line-height:1em">Protokol Kesehatan</h4>
+                        </div>
+                        <div class="card-body">
+                            <p class="text-justify">Sehubungan dengan situasi pandemi COVID-19 saat ini, tanpa
+                                mengurangi rasa hormat kami kepada Bapak/Ibu/Saudara/Saudari, acara Tasyakuran Aqiqah
+                                akan dilaksanakan dengan menerapkan standar protokol kesehatan.</p>
+                            <div class="" data-aos="zoom-in">
+                                <img src="https://undangkita.com/gallery/covid-prokes.jpg" class="rounded shadow-sm" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="location" class="col-12 col-sm-8 pt-5" data-aos="fade-left">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1991.3570508808239!2d119.8073559!3d-3.419666!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d9413c68f689a95%3A0x21a1b870b044a501!2sMasjid%2FMushollah!5e0!3m2!1sid!2sid!4v1666023918735!5m2!1sid!2sid"
+                        width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    <p class="mt-3">
+                        <a class="btn btn-light" href="https://goo.gl/maps/rkAJ3exJ5uvJz6JZA">Buka Maps</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+
+    <div id="guestbook" class="py-5 bg-light">
+       
+
+    <div class="py-5 bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 offset-md-2 text-center" data-aos="fade-down">
+                    <p>Hormat Kami yang Mengundang</p>
+                    <p class="font-weight-bold">Andi Hermanto & Nur Afifa dan Sekeluarga</p>
+                </div>
+            </div>
+
+            <div height="0" width="0">
+                <audio id="mysong" autoplay="autoplay" loop="loop" preload="preload">
+                    <source src="https://undangkita.com/gallery/Aviwkila - Lahir reff.mp3" type="audio/mpeg">
+                    </source>
+                </audio>
+            </div>
+        </div>
+    </div>
+
+    
+
+
+<style>
+    .sharesocial ul li a.homeup {
+        color: #343a40;
+    }
+
+
+    @media (max-width:576px) {
+        .sharesocial ul li {
+            float: left;
+            width: 33.33% !important;
+            color: #343a40;
+            text-align: center;
+        }
+    }
+</style>
+
+<script>
+    var on = document.getElementById("on");
+    var off = document.getElementById("off");
+    var xsong = document.getElementById("mysong");
+
+    function playAudio() {
+        xsong.play();
+        document.getElementById("on").style.display = "inline-block";
+        document.getElementById("off").style.display = "none";
+    }
+
+    function pauseAudio() {
+        xsong.pause();
+        document.getElementById("off").style.display = "inline-block";
+        document.getElementById("on").style.display = "none";
+    }
+</script>
+    <!-- footer open -->
+ 
+    <!-- footer end -->
+
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h6 class="modal-title">Aqiqah Anak Kami</h6>
+                </div>
+                <div class="modal-body">
+                    <p class="mb-2">Kepada Yth. Bapak/Ibu/Saudara/i</p>
+                    <h6>Di Tempat</h6>                    <p>Kami mengundang Anda untuk berkenan hadir di acara Aqiqah anak kami.</p>
+                </div>
+                <div class="modal-footer">
+               
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="playAudio()">Buka
+                        Undangan</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function () {
+            // Show the Modal on load
+            $("#myModal").modal("show");
+        });
+    </script>
+
+    <style>
+        .pacifico {
+            font-family: 'Pacifico', cursive;
+            font-size: 40px;
+            line-height: .9em;
+        }
+
+        .pacifico2 {
+            font-family: 'Pacifico', cursive;
+            font-size: 40px;
+            line-height: .9em;
+            font-weight: normal;
+        }
+
+        .pacifico3 {
+            font-family: 'Pacifico', cursive;
+        }
+
+        .raleway {
+            font-family: 'Raleway', sans-serif;
+            font-weight: 700;
+            line-height: 1em;
+        }
+
+        .raleway2 {
+            font-family: 'Raleway', sans-serif;
+            line-height: .9em;
+            font-weight: 900;
+        }
+
+        .btn {
+            font-size: 14px;
+        }
+
+        .bgnav {
+            background: rgba(232, 245, 233, .1);
+        }
+
+        .bgcov {
+            height: 100%;
+            width: 100%;
+            background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(255, 255, 255, 0.8)), url(http://nazeeyaaynazhandini.site/zeeya22.jpg);
+            background-position: center;
+            background-repeat: no-repeat;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            background-size: cover;
+            position: relative;
+            height: 100vh;
+            overflow: hidden;
+            z-index: 1;
+        }
+
+        .introcov {
+            color: #343a40;
+            width: 600px;
+            max-width: 100%;
+            margin: auto;
+            position: absolute;
+            bottom: 10%;
+            left: 50%;
+            -webkit-transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+            text-shadow: 0px 1px 2px rgba(0, 0, 0, .3);
+        }
+
+        .bgsection {
+            background: #ffafbf url(http://nazeeyaaynazhandini.site/nahh.jpg) repeat;
+            /*pink terang*/
+        }
+
+        .bgimage {
+            background: linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(../gallery/wreath1d.png) no-repeat top left 50px, #f8f9fa;
+            background-attachment: fixed;
+        }
+
+        .bgdate {
+            background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url(http://nazeeyaaynazhandini.site/nahh.jpg);
+            background-position: center;
+            background-repeat: no-repeat;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            background-size: cover;
+            background-attachment: fixed;
+        }
+
+        .bgtime {
+            background: -webkit-repeating-linear-gradient(45deg, #ffafbf, #ffafbf 30px, #ff7b9e 30px, #ff7b9e 60px);
+            background: repeating-linear-gradient(45deg, #ffafbf, #ffafbf 30px, #ff7b9e 30px, #ff7b9e 60px);
+        }
+
+        .bgtime ul {
+            color: #333;
+            text-align: center;
+        }
+
+        .bgtime li {
+            background: #f8f9fa;
+            display: inline-block;
+            font-size: 1em;
+            list-style-type: none;
+            padding: 1em;
+            margin: 3px;
+            text-transform: uppercase;
+        }
+
+        .bgtime ul li span {
+            display: block;
+            font-size: 2rem;
+        }
+
+        @media (max-width:576px) {
+            .pacifico {
+                font-size: 30px;
+            }
+
+            .pacifico2 {
+                font-size: 25px;
+            }
+
+            .bgtime ul li {
+                font-size: .8em;
+                padding: .5em;
+            }
+        }
+
+        /* GuestBook */
+        #guestbook .btn {
+            background: #ffafbf;
+            border-color: #ff7b9e;
+        }
+
+        .formchat {
+            color: #343a40;
+            background-color: #ffafbf;
+            border: 1px solid #ff7b9e;
+            max-height: 205px;
+            overflow: auto;
+        }
+    </style>
+
+    <!-- Optional JavaScript -->
+    <script src="https://undangkita.com/js/jquery.fancybox.min.js"></script>
+    <script src="https://undangkita.com/js/smooth-scroll.polyfills.min.js"></script>
+    <script src="https://undangkita.com/js/aos.js"></script>
+    <script src="https://undangkita.com/js/addscript.js"></script>
+    <script>
+        const second = 1000,
+            minute = second * 60,
+            hour = minute * 60,
+            day = hour * 24;
+
+        let countDown = new Date('Oct 23, 2022 09:00:00').getTime(),
+            x = setInterval(function () {
+
+                let now = new Date().getTime(),
+                    distance = countDown - now;
+
+                document.getElementById('days').innerText = Math.floor(distance / (day)),
+                    document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+                    document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+                    document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+            }, second)
+    </script>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://undangkita.com/js/bootstrap.min.js"></script>
+    <script src="https://undangkita.com/js/redirect.js"></script>
+</body>
+
+</html>
